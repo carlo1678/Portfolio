32eaf6369bd0a8786d453bacb396eb93aada1d6b
Carlo Carbonell <carlo1678@hotmail.com>
2021-06-24 12:35:10 -0400

    fixed form and sidebar

--- tunis/tunis-personal-portfolio/dark/php/process-form.php
+++ tunis/tunis-personal-portfolio/dark/php/process-form.php
@@ -7,7 +7,7 @@ if (isset($_REQUEST['name'],$_REQUEST['email'])) {
     $message = $_REQUEST['message'];
 
     // Set your email address where you want to receive emails.
-    $to = 'yourmail@mail.com';
+    $to = 'carlo1678@hotmail.com';
 
     $headers = "From: ".$name." <".$mail."> \r\n";
 
